0b78b065ffbb56ba7b93f10cc9b6e0f494b075f3
Rohit Verma <28vrohit@gmail.com>
2023-12-03 17:41:07 +0530

    .

--- components/Register/Register.jsx
+++ components/Register/Register.jsx
@@ -1,22 +1,12 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import { View, Text } from "react-native";
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
-
-const Tab = createBottomTabNavigator();
+import { View, Text, SafeAreaView, StatusBar } from "react-native";
 
 export default function Register() {
   return (
-    <NavigationContainer independent={true}>
-      <Tab.Navigator
-        screenOptions={{ headerShown: false }}
-        tabBarShowLabel={false}
-      >
-        <Tab.Screen name="SignIn" component={SignIn} />
-        <Tab.Screen name="SignUp" component={SignUp} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>Register</Text>
+      </View>
+    </SafeAreaView>
   );
 }
--- navigation/AppNavigation.jsx
+++ navigation/AppNavigation.jsx
@@ -1,17 +1,184 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+const Tab = createBottomTabNavigator();
 
-import HomeScreen from "../screens/HomeScreen";
 import Register from "../components/Register/Register";
-
-const Stack = createNativeStackNavigator();
-
+import { View, Image, Text, TouchableOpacity } from "react-native";
 export default function AppNavigation() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Register} />
-      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: "absolute",
+            bottom: 25,
+            left: 20,
+            right: 20,
+            elevation: 5,
+            height: 90,
+            borderRadius: 15,
+            shadowColor: "#7F5D0",
+            shadowOffset: {
+              width: 0,
+              height: 10,
+            },
+            shadowOpacity: 0.25,
+            shadowRadius: 3.5,
+          },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Register}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: 10,
+                }}
+              >
+                <Image
+                  source={require("../assets/images/coffee1.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? "#e32f45" : "#748c94",
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: focused ? "#e32f45" : "#748c94",
+                  }}
+                >
+                  HOME
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home1"
+          component={Register}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: 10,
+                }}
+              >
+                <Image
+                  source={require("../assets/images/coffee2.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? "#e32f45" : "#748c94",
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: focused ? "#e32f45" : "#748c94",
+                  }}
+                >
+                  HOME1
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home2"
+          component={Register}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("../assets/images/coffee4.png")}
+                resizeMode="contain"
+                style={{
+                  width: 50,
+                  height: 50,
+                  top: -30,
+                }}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home3"
+          component={Register}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: 10,
+                }}
+              >
+                <Image
+                  source={require("../assets/images/coffee4.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? "#e32f45" : "#748c94",
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: focused ? "#e32f45" : "#748c94",
+                  }}
+                >
+                  HOME3
+                </Text>
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Home4"
+          component={Register}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <View
+                style={{
+                  alignItems: "center",
+                  justifyContent: "center",
+                  top: 10,
+                }}
+              >
+                <Image
+                  source={require("../assets/images/coffee5.png")}
+                  resizeMode="contain"
+                  style={{
+                    width: 25,
+                    height: 25,
+                    // tintColor: focused ? "#e32f45" : "#748c94",
+                  }}
+                />
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: focused ? "#e32f45" : "#748c94",
+                  }}
+                >
+                  HOME4
+                </Text>
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
