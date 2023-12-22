68bc34994d6faffd25e7ef5feeadf9c2a1fa33bd
Rohit Verma <28vrohit@gmail.com>
2023-12-22 12:06:59 +0530

    .

--- components/Post/Posts.jsx
+++ components/Post/Posts.jsx
@@ -1,18 +1,29 @@
-import { View, Text, SafeAreaView, StatusBar, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StatusBar,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 import PostList from "./PostList";
 
 export default function Posts() {
+  const windowHeight = Dimensions.get("window").height;
   return (
     <SafeAreaView>
       <StatusBar />
-      <ScrollView className="h-[32rem]">
+      <ScrollView
+        style={{
+          height: windowHeight - 150,
+        }}
+      >
         {new Array(3).fill().map((item, index) => (
           <View key={index}>
             <PostList />
           </View>
         ))}
       </ScrollView>
-      <Text>wedfghj</Text>
     </SafeAreaView>
   );
 }
